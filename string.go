fbf30e2090709c88d91fbd109e40afaf92db647c
Geraldie Tanu Saputra <geraldietanusaputra@gmail.com>
2021-01-06 15:13:37 +0700

    print string and simple string operation

--- string.go
+++ string.go
@@ -7,4 +7,8 @@ func main() {
 	fmt.Println("Golang")
 	fmt.Println("This is very super most absolutely incredibly extraordinary extremely ultimately string that formed from combined many characters and spaces. This is also including symbols, oops... These are symbols: ~!@#$%^&*()_+{}:<>?. To use Quotation Mark, we can use backslash symbol, like this \", tadaaa..... And now lets print this very most super ult... ahh.. whatever string")
 
+	fmt.Println(len("Geraldie"))
+	fmt.Println("Golang"[4]) //Using index array notation in String will produce Byte of charater which pointed by index. In this string, byte of 'n' (index 4) is 110.
+	fmt.Println("Below this string is length of super string above")
+	fmt.Println(len("This is very super most absolutely incredibly extraordinary extremely ultimately string that formed from combined many characters and spaces. This is also including symbols, oops... These are symbols: ~!@#$%^&*()_+{}:<>?. To use Quotation Mark, we can use backslash symbol, like this \", tadaaa..... And now lets print this very most super ult... ahh.. whatever string"))
 }
