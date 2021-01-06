efdf741d5b28dffe6128d0f8e2ab5fd17e1a9241
Geraldie Tanu Saputra <geraldietanusaputra@gmail.com>
2021-01-06 15:00:09 +0700

    print string

---
+++ string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Geraldie")
+	fmt.Println("Golang")
+	fmt.Println("This is very super most absolutely incredibly extraordinary extremely ultimately string that formed from combined many characters and spaces. This is also including symbols, oops... These are symbols: ~!@#$%^&*()_+{}:<>?. To use Quotation Mark, we can use backslash symbol, like this \", tadaaa..... And now lets print this very most super ult... ahh.. whatever string")
+
+}
