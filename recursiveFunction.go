0a7dab2c02b0e04bebcc7e4255500569deb4ee9e
Geraldie Tanu Saputra <geraldietanusaputra@gmail.com>
2021-01-07 18:37:49 +0700

    recursive function

--- recursiveFunction.go
+++ recursiveFunction.go
@@ -5,9 +5,8 @@ import "fmt"
 func factorial(value int) int {
 	if value == 0 || value == 1 {
 		return 1
-	} else {
-		return value * factorial(value-1)
 	}
+	return value * factorial(value-1)
 }
 
 func main() {
